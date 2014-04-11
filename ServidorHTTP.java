ee0ef41db7ab36321902bb314a7a93f0c05d22e3
DanielRivera0804 <daniel.riverat@alumnos.usm.cl>
2014-04-11 10:38:44 -0300

    Create ServidorHTTP.java

---
+++ ServidorHTTP.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class ServidorHTTP
+{
+	
+	int puerto = 90;
+	
+	final int ERROR = 0;
+	final int WARNING = 1;
+	final int DEBUG = 2;
+	
+	
+	public static void main(String[] array)
+	{
+		ServidorHTTP instancia = new ServidorHTTP(array);
+		instancia.arranca();
+	}
+	
+	ServidorHTTP(String[] param)
+	{
+		procesaParametros();
+	}
+	
+	boolean procesaParametros(){
+		
+		return true;
+	}
+	
+	boolean arranca(){
+		
+		System.out.println("Arrancamos nuestro servidor");
+		try
+		{
+				ServerSocket s = new ServerSocket(90);
+				System.out.println("Quedamos a la espera de conexion");
+						
+				while(true)
+				{ // Bucle Infinito
+						Socket entrante = s.accept();		
+						peticionWeb pCliente = new peticionWeb(entrante);
+						pCliente.start();
+				}				
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error en servidor\n" + e.toString());
+		}
+			return true;
+		}
+		
+	}
+	
+	
+	
+	
+	

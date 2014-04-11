7023d6712d503db33f7618d298b41484247be4c7
DanielRivera0804 <daniel.riverat@alumnos.usm.cl>
2014-04-11 14:37:07 -0300

    Update peticionWeb.java

--- peticionWeb.java
+++ peticionWeb.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -36,63 +39,63 @@ class peticionWeb extends Thread
 
 	public void run() // emplementamos el metodo run
 	{
-		System.out.println("Procesamos conexion");
-
+		System.out.println("[Procesamos la Conexion]");
+		
 		try
 		{
 			BufferedReader in = new BufferedReader(new InputStreamReader(scliente.getInputStream()));
 			out = new PrintWriter (new OutputStreamWriter(scliente.getOutputStream(),"8859_1"),true) ;
 			
-			String cadena = "";
+			String cadena = "default";
 			int i = 0;
 			
 			
-			do
+			while(cadena != null && cadena.length() != 0)
 			{
+				//SE LEE UNA LINEA DE CADENA
 				cadena = in.readLine();
-
-				if (cadena != null )
+				System.out.println("-- " + cadena);
+				
+				if(i == 0)
 				{
-					// sleep(500);
-					depura("--" + cadena + "-");
+					i++;
+					
+					StringTokenizer st = new StringTokenizer(cadena);
+					
+					if( st.countTokens() >= 2 )
+					{
+						String method = st.nextToken();
+						
+						if( method.equals("GET"))
+						{
+							retornaFichero(st.nextToken());
+							
+						}
+						else if(method.equals("POST"))
+						{
+							procesarPost(in,st.nextToken());
+							break;
+						}
+						else
+						{
+							System.out.println("[400 Peticion Incorrecta]");
+						}
+
+					}
 				}
-			
-
-	     		  if(i == 0) // la primera linea nos dice que fichero hay que descargar
-			  {
-			        i++;
-
-			        StringTokenizer st = new StringTokenizer(cadena);
-
-			        if ((st.countTokens() >= 2) && st.nextToken().equals("GET"))
-              		 {
-              			 retornaFichero(st.nextToken()) ;
-              		 }
-			        else if((st.countTokens() >= 2) && st.nextToken().equals("POST"))
-             		 {
-             			 System.out.println("POST MADAFACA");
-             		 }
-	              	else
-	             	 {
-	             		   	out.println("400 Petición Incorrecta") ;
-	                 }
-			    }
-
 			}
-			while (cadena != null && cadena.length() != 0);
-
+			
 		}
 		catch(Exception e)
 		{
 			System.out.println("Error en servidor\n" + e.toString());
 		}
-
-		System.out.println("Hemos terminado");
 	}
-	
+
+	//Funcion que nos otorga recuperar el
+	//Archivo solicitado por GET
 	void retornaFichero(String sfichero)
 	{
-		
 
 		// comprobamos si tiene una barra al principio
 		if(sfichero.equals("/favicon.ico") )
@@ -148,7 +151,7 @@ class peticionWeb extends Thread
 				ficheroLocal.close();
 				out.close();
 
-			}  // fin de si el fiechero existe
+			}  // fin de si el fichero existe
 			else
 			{
 				depura("No encuentro el fichero " + mifichero.toString());
@@ -159,5 +162,67 @@ class peticionWeb extends Thread
 			depura("Error al retornar fichero");
 		}
 	}
+	
+	void procesarPost(BufferedReader in,String sfichero) throws IOException
+	{
+		String cadena = "default";
+		int length = 0; //Aca se guarda el largo de caracteres que contiene el POST
+		while( cadena.equals("") == false)
+		{
+			cadena = in.readLine();
+			
+			//Se extrae el largo del POST
+			if(cadena.startsWith("Content-Length"))
+			{
+				length = Integer.parseInt(cadena.substring(16));
+			}
+			System.out.println("#"+cadena);
+		}
+		
+		//-------------------------------------------------
+		// Se Llega hasta la linea NULL, 
+		//...ahora se deben rescatar los datos ingresados
+		//-------------------------------------------------
+		
+		int j = length;
+		
+		//Creamos el String 'query' con el arreglo nombre/valor con los chars correspondientes
+		String  query= "";
+		while( j != 0 ){
 
+			query = query.concat(Character.toString((char)in.read()));
+			j--;
+		}
+	
+		guardarContacto(query);// El query se guarda en Contactos.txt con la funcion guardarcontacto
+		retornaFichero(sfichero); // Se retorna a la pagina 
+	}
+	
+	
+	
+	//Esta funcion guarda los datos en Contactos.txt
+	//Cada linea representa un usuario
+	void guardarContacto(String query) throws IOException
+	{
+		File f = new File("Contactos.txt");
+		
+		if(f.exists()) //Si el archivo con los contactos existen
+		{
+			FileWriter w = new FileWriter(f,true);
+			BufferedWriter bw = new BufferedWriter(w);
+			PrintWriter wr = new PrintWriter(bw); 
+
+			String[] s = query.split("&");
+			String usuario = s[0].substring(s[0].indexOf("=") + 1,s[0].length());
+			String ip = s[1].substring(s[1].indexOf("=") + 1,s[1].length());
+			String puerto = s[2].substring(s[2].indexOf("=") + 1,s[2].length());
+			
+			wr.append(usuario + "," + ip + ","+ puerto + "\n");
+			wr.close();
+			bw.close();
+			
+		}
+		
+	}
 }
+
